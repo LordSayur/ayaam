e57f59bbc40a08934058675f29005422a2f9d5e3
Lord Sayur L <umar_317@hotmail.com>
2019-08-16 17:22:32 +0800

    add awang bro logics

---
+++ ayaaam/Assets/scripts/GrabberController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GrabberController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Chicken") 
+        {
+            AyamController ayam = other.GetComponentInParent<AyamController>();
+            ayam.DeactivateAyam();
+            ayam.enabled = false;
+            
+            other.transform.parent.SetParent(gameObject.transform);
+            other.transform.parent.transform.localPosition = Vector3.zero;
+        }
+    }
+}
