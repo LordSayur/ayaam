c204e0330587b5600bd42a9cd419625b2f9b1afe
Lord Sayur <umar_317@hotmail.com>
2019-11-17 16:45:08 +0800

    Create scene controller logic

---
+++ ayaaam/Assets/scripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private int MapIndex;
+
+    private void Start()
+    {
+        MapIndex = GetComponent<MapController>().currentMapIndex + 1;
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadSelectedMap()
+    {
+        SceneManager.LoadScene(GetComponent<MapController>().currentMapIndex + 1);
+    }
+}
