0cb4bf2a3f2f0c25abff32fcf3526d7b0a8fa3e0
Lord Sayur <umar_317@hotmail.com>
2019-11-17 16:45:59 +0800

    Create map controller logic script

---
+++ ayaaam/Assets/scripts/MapController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Maps;
+    [HideInInspector] public int currentMapIndex = 0;
+    private int previousMapIndex = 0;
+
+    private void Start()
+    {
+        Maps[currentMapIndex].SetActive(true);
+    }
+
+    public void ChangeMap()
+    {
+
+        previousMapIndex = currentMapIndex;
+        if(Maps.Length == currentMapIndex + 1)
+        {
+            currentMapIndex = 0;
+        }
+        else
+        {
+            currentMapIndex += 1;
+        }
+
+        Maps[previousMapIndex].SetActive(false);
+        Maps[currentMapIndex].SetActive(true);
+    }
+}
