85117c6b021fd5b20575f68a7714f81a2816663f
Lord Sayur <umar_317@hotmail.com>
2019-11-17 17:29:47 +0800

    Create Game Controller script

---
+++ ayaaam/Assets/scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int m_ayam_count = 0;
+
+    public void AddAyamCount()
+    {
+        m_ayam_count++;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
